31507ff5a4f200b25b331bbb635150340a8cd388
Tom Adamski <tomadamski19@gmail.com>
2019-04-16 19:29:59 +0200

    Storing songs in Dexie db

--- js/db.js
+++ js/db.js
@@ -1,4 +1,9 @@
 var db = new Dexie("music_database");
-          db.version(1).stores({
-              artists: 'id,name,image_url'
-          });
+db.version(1).stores({
+    artists: 'id,name,image_url',
+});
+db.version(2).stores({
+    artists: 'id,name,image_url',
+    songs: 'id,title, artist_name,image_url,feat_name'
+});
+          
--- js/feed.js
+++ js/feed.js
@@ -4,52 +4,40 @@ var songsSuffix = "/songs";
 
 var accessToken = "rQtoe-mTiZO_EFGvaopmbn3eWKnMyolMAL_m7XzQyZkgxAeeM3zsU_sRfzKrmKYC";
 
-var loadingCount = 0;
-var songs = [];
+//var loadingCount = 0;
+//var songs = [];
 
 
 function loadFetchDisplay(){
-    db.artists.toArray(array => fetchSongs(array));
+    db.artists.toArray(artists => fetchSongs(artists));
 }
 
-function createArtistsArray(resultJson){
-    var artists = [];
-    for(var i in resultJson){
-        var artist = {
-            name: resultJson[i]['name'],
-            id: resultJson[i]['id'],
-            image_url: resultJson[i]['image-url']
-        };
-        artists.push(artist);
-    }
-    return artists;
+
+//TODO Use loadingCount to check when update is finished
+function update(){
+    db.artists.toArray(function(artists){
+        songs = [];
+        for(var i in artists){
+            (function (i) {
+                //loadingCount++; //permet d'afficher seulement une fois que tous les artistes ont leur chanson chargée
+                var featName = artists[i]['name'];
+
+                $.ajax({
+                    method: "GET",
+                    crossDomain:true,
+                    url: artistsUrl + artists[i]['id'] + songsSuffix,
+                    data: { page: "1", sort:"release_date", per_page:"1", access_token:accessToken}
+                })
+                    .done(function(json) {
+                        // when executing AJAX the iterator is already at the end, sending the wrong feat name
+                        saveSong(json, featName);
+                });    
+            })(i); 
+        }
+    });
 }
 
-function fetchSongs(artists){
-
-    songs = [];
-    // Pour chaque artiste
-    for(var i in artists){
-        (function (i) {
-            loadingCount++; //permet d'afficher seulement une fois que tous les artistes ont leur chanson chargée
-            var featName = artists[i]['name'];
-
-            $.ajax({
-                method: "GET",
-                crossDomain:true,
-                url: artistsUrl + artists[i]['id'] + songsSuffix,
-                data: { page: "1", sort:"release_date", per_page:"1", access_token:accessToken}
-            })
-                .done(function(json) {
-                    // when executing AJAX the iterator is already at the end, sending the wrong feat name
-                    addToSongsArray(json, featName);
-            });    
-        })(i); 
-    }
-}
-
-function addToSongsArray(json, featName){
-
+function saveSong(json,featName){
     var hits = json.response.songs;
     for(var i in hits){
         
@@ -61,11 +49,13 @@ function addToSongsArray(json, featName){
         };
 
         if(song.artist_name != featName)
-            song['feat_name']= featName;
-
+            song['feat_name'] = featName;
+        else
+            song['feat_name'] = null;
         getDateAddToArray(song);
     }
 }
+
 
 function getDateAddToArray(song){
 
@@ -77,55 +67,64 @@ function getDateAddToArray(song){
       })
         .done(function( json ) {
             song['release_date'] = json['response']['song']['release_date'];
-            songs.push(song);
-            loadingCount--;
-            displaySongs();
+
+            db.songs.put({
+                id: song['id'],
+                title: song['title'],
+                artist_name: song['artist_name'],
+                image_url: song['image_url'],
+                feat_name: song['feat_name']
+            });
+            //loadingCount--;
     }); 
 }
 
-function displaySongs(){
-    if(loadingCount == 0){ // toutes les chansons ont été chargées
-        $("#list-container").empty();
-        console.log(songs);
-        // Création de chaque encart de chansons
-        for(var i in songs){
+function display(){
+    db.songs.toArray(array => displaySongs(array));
+}
 
-            //song container
-            var row = $('<div/>', {class: 'row border center-items' });
+function displaySongs(songs){
+    $("#list-container").empty();
+    // Création de chaque encart de chansons
+    for(var i in songs){
 
-            //content values
-            var id = songs[i]['id'], title = songs[i]['title'], 
-                url = songs[i]['image_url'], release_date = songs[i]['release_date']
-                artist_name = songs[i]['artist_name'], feat_name = null;
+        //song container
+        var row = $('<div/>', {class: 'row border center-items' });
 
-            if(songs[i]['feat_name'] != null)
-                feat_name = songs[i]['feat_name'];
+        //content values
+        var id = songs[i]['id'], 
+            title = songs[i]['title'], 
+            url = songs[i]['image_url'], release_date = songs[i]['release_date']
+            artist_name = songs[i]['artist_name'], feat_name = null;
+
+        if(songs[i]['feat_name'] != null)
+            feat_name = songs[i]['feat_name'];
 
 
-            //content fields
-            var image = $('<div/>', {class: 'col-sm-2 nopadding' })
-                    .append($('<img/>',{class: 'img-fluid', src:url, }));
-            var contentField = $('<div/>', {class: 'col-sm-10' });
+        //content fields
+        var image = $('<div/>', {class: 'col-sm-2 nopadding' })
+                .append($('<img/>',{class: 'img-fluid', src:url, }));
+        var contentField = $('<div/>', {class: 'col-sm-10' });
 
-            var titleField = $('<div/>', {class: 'row' }).append("Titre : "+title);
-            var artistField = $('<div/>', {class: 'row' }).append("Artiste : "+artist_name); 
-            var dateField = $('<div/>', {class: 'row' }).append("Sortie : "+release_date);
+        var titleField = $('<div/>', {class: 'row' }).append("Titre : "+title);
+        var artistField = $('<div/>', {class: 'row' }).append("Artiste : "+artist_name); 
+        var dateField = $('<div/>', {class: 'row' }).append("Sortie : "+release_date);
 
-            contentField.append(titleField).append(artistField).append(dateField);
-            
-            if(feat_name != null){
-                var featField = $('<div/>', {class: 'row' }).append("Featuring : "+feat_name);
-                contentField.append(featField)
-            }
-            
-            row.append(image);
-            row.append(contentField);
+        contentField.append(titleField).append(artistField).append(dateField);
         
-            $("#list-container").append(row);
-        
+        if(feat_name != null){
+            var featField = $('<div/>', {class: 'row' }).append("Featuring : "+feat_name);
+            contentField.append(featField)
         }
-
-
-
+        
+        row.append(image);
+        row.append(contentField);
+    
+        $("#list-container").append(row);
+    
     }
+}
+
+function flush(){
+    db.songs.clear();
 }
